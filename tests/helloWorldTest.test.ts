8ef1bfca46781c9e08cdec24a191fc7a06ab7743
kostiantynn <kostya.code@gmail.com>
2021-03-04 08:54:53 +0200

    establishing tests

---
+++ tests/helloWorldTest.test.ts
@@ -0,0 +1,8 @@
+import { suite, test } from '@testdeck/mocha';
+import * as _chai from 'chai';
+import { mock, instance } from 'ts-mockito';
+_chai.should();
+@suite class HelloWorldServiceUnitTests {
+
+
+}
