fa66182c912cb25c5dfd4af1399e624ed9722d69
kostiantynn <kostya.code@gmail.com>
2021-05-26 23:43:24 +0300

    update cors settings

--- src/server/index.ts
+++ src/server/index.ts
@@ -13,7 +13,7 @@ const routers: any[] = [contest, user, submission, comment, attachment, auth];
 
 export default (server: FastifyInstance<Server>) => {
   server.register(require('fastify-cors'), {
-    origin: 'localhost',
+    origin: 'true',
   });
   server.register(require('fastify-cookie'), {
     secret: process.env.COOKIE_SECRET,
